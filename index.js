86fac7ea24caca57405143a9a0b6f3ac7e6d7db9
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-20 10:46:32 -0800

    commit tree 36ea1703ba3596b3644748cdd66fbfe7cdd61f94

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function mapToNegativize(arr) {
+  let newArr = [];
+  arr.forEach((ele) => {
+    newArr.push(ele * -1);
+  });
+  return newArr;
+}
+
+function mapToNoChange(arr) {
+  let newArr = [];
+  arr.forEach((ele) => {
+    newArr.push(ele);
+  });
+  return newArr;
+}
+
+function mapToDouble(arr) {
+  let newArr = [];
+  arr.forEach((ele) => {
+    newArr.push(ele * 2);
+  });
+  return newArr;
+}
+function mapToSquare(arr) {
+  let newArr = [];
+  arr.forEach((ele) => {
+    newArr.push(ele * ele);
+  });
+  return newArr;
+}
+
+function reduceToTotal(arr, start = 0) {
+  arr.forEach((ele) => {
+    start += ele;
+  });
+  return start;
+}
